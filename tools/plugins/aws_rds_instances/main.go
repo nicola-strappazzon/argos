a849e6e5c0d9c3c5fabca47350a9f888e12f2873
Nicola Strappazzon. <nicola.strappazzon@mylighthouse.com>
2026-02-28 21:11:47 +0100

    add PerformanceInsights field on aws_rds_instances

--- tools/plugins/aws_rds_instances/main.go
+++ tools/plugins/aws_rds_instances/main.go
@@ -17,14 +17,15 @@ type Endpoint struct {
 }
 
 type Instance struct {
-	Identifier    string   `json:"identifier"`
-	Class         string   `json:"class"`
-	Status        string   `json:"status"`
-	Engine        string   `json:"engine"`
-	EngineVersion string   `json:"engine_version"`
-	AZ            string   `json:"availability_zone"`
-	MultiAZ       bool     `json:"multi_az"`
-	Endpoint      Endpoint `json:"endpoint"`
+	Identifier                 string   `json:"identifier"`
+	Class                      string   `json:"class"`
+	Status                     string   `json:"status"`
+	Engine                     string   `json:"engine"`
+	EngineVersion              string   `json:"engine_version"`
+	AZ                         string   `json:"availability_zone"`
+	MultiAZ                    bool     `json:"multi_az"`
+	Endpoint                   Endpoint `json:"endpoint"`
+	PerformanceInsightsEnabled bool     `json:"performance_insights_enabled"`
 }
 
 func init() {
@@ -47,13 +48,14 @@ func init() {
 			instances := make([]Instance, 0, len(result.DBInstances))
 			for _, db := range result.DBInstances {
 				inst := Instance{
-					Identifier:    aws.StringValue(db.DBInstanceIdentifier),
-					Class:         aws.StringValue(db.DBInstanceClass),
-					Status:        aws.StringValue(db.DBInstanceStatus),
-					Engine:        aws.StringValue(db.Engine),
-					EngineVersion: aws.StringValue(db.EngineVersion),
-					AZ:            aws.StringValue(db.AvailabilityZone),
-					MultiAZ:       aws.BoolValue(db.MultiAZ),
+					Identifier:                 aws.StringValue(db.DBInstanceIdentifier),
+					Class:                      aws.StringValue(db.DBInstanceClass),
+					Status:                     aws.StringValue(db.DBInstanceStatus),
+					Engine:                     aws.StringValue(db.Engine),
+					EngineVersion:              aws.StringValue(db.EngineVersion),
+					AZ:                         aws.StringValue(db.AvailabilityZone),
+					MultiAZ:                    aws.BoolValue(db.MultiAZ),
+					PerformanceInsightsEnabled: aws.BoolValue(db.PerformanceInsightsEnabled),
 				}
 
 				if db.Endpoint != nil {
